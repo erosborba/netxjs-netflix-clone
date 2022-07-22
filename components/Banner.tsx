9ad8de61367f8a62b6e6d8c4689d9ce0839fd42e
Eros Borba <erosborba@pop-os.localdomain>
2022-07-21 21:25:24 -0300

    debug index

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -20,33 +20,29 @@ function Banner({ netflixOriginals }: Props) {
   return (
     console.log("banner-log", movie, netflixOriginals),
     (
-     
-        
-        <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
-          <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
-            <img
-              src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
-            />
-          </div>
-
-          <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl drop-shadow-md ">
-            {movie?.title || movie?.name || movie?.original_name}
-          </h1>
-          <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl drop-shadow-md ">
-            {movie?.overview}
-          </p>
-          <div className="flex space-x-3 drop-shadow-md ">
-            <button className="bannerButton bg-white text-black">
-              <FaPlay className="h-4 w-4 text-black" /> Play
-            </button>
-            <button className="bannerButton bg-[gray]/70">
-              Mais Info
-              <InformationCircleIcon className="h-5 w-5 md:h-8 md:w8" />
-            </button>
-          </div>
+      <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+        <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+          <img
+            src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          />
         </div>
-       
-      
+
+        <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl drop-shadow-md ">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl drop-shadow-md ">
+          {movie?.overview}
+        </p>
+        <div className="flex space-x-3 drop-shadow-md ">
+          <button className="bannerButton bg-white text-black">
+            <FaPlay className="h-4 w-4 text-black" /> Play
+          </button>
+          <button className="bannerButton bg-[gray]/70">
+            Mais Info
+            <InformationCircleIcon className="h-5 w-5 md:h-8 md:w8" />
+          </button>
+        </div>
+      </div>
     )
   );
 }
